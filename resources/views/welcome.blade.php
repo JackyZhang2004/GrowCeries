2427797b65b1031ae7accbf8dd0bdf1610eefaaa
Interblues88 <delvin.setiamin.08@gmail.com>
2024-04-25 13:38:41 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel tes</title>
+        <title>Laravel nyoba</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">

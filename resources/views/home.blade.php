e25e7684345451f0bea68ce8ff4bd7d748159b30
Interblues88 <delvin.setiamin.08@gmail.com>
2024-05-06 16:22:13 +0700

    making discover page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,7 @@
     {{-- untuk navbarnya udah ada dari layouts.app, jadi langsung ngoding bagian home yang jumbotron --}}
     {{-- kalo mau ngoding dimulai dari sini --}}
  
-    <section class="bg-center bg-no-repeat bg-[url('image/backgroundCarousel.png')]" id="searchJumbotron">
+    {{-- <section class="bg-center bg-no-repeat bg-[url('image/backgroundCarousel.png')]" id="searchJumbotron">
       <div class="px-4 mx-auto text-center">
           <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
           <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
@@ -45,7 +45,44 @@
               </a>  
           </div>
       </div>
-    </section>
+    </section> --}}
+    {{-- <div class="backgroundSearch">
+      <div class = "kiri">
+        <h1>apanih</h1>
+      </div>
+      <div class="kanan">
+        <img src="image/backgroundCarousel.png" alt="" id="gambarCarousel">
+      </div>
+    </div> --}}
+
+    <div class="backgroundSearch">
+      <div class = "kiri">
+        <h1>apanih</h1>
+      </div>
+      {{-- <div class="kanan"> --}}
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="1">
+              <img class="d-block w-100" src="image/backgroundCarousel.png" alt="First slide" id="gambarCarousel">
+            </div>
+            <div class="carousel-item" data-bs-interval="1">
+              <img class="d-block w-100" src="image/backgroundCarousel.png" alt="Second slide" id="gambarCarousel">
+            </div>
+            <div class="carousel-item" data-bs-interval="1">
+              <img class="d-block w-100" src="image/backgroundCarousel.png" alt="Third slide" id="gambarCarousel">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      {{-- </div> --}}
+    </div>
 
     {{-- Recommended Section --}}
     <div class="recommendedSection">

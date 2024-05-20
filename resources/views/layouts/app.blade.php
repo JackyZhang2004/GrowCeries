914a9367d114c7c97faffd3cf5bea6a385046269
AthaliaYechiel <122966304+AthaliaYechiel@users.noreply.github.com>
2024-05-20 22:26:28 +0700

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> @yield('title') </title>    
+    <title> @yield('title') </title>
 
     {{-- Bootstrap CSS & JS--}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -30,10 +30,12 @@
     @livewireStyles
 </head>
 <body>
+    <livewire:nav>
+    @yield('content')
     @if (request()->is('login') || request()->is('register'))
        <livewire:nav-login-register>
     @else
-        <livewire:nav>   
+        <livewire:nav>
     @endif
 
 
@@ -50,16 +52,16 @@
             @yield('success')
             @yield('login')
     @else
-        @yield('content') 
+        @yield('content')
     @endif
 
     <livewire:footer>
-    @livewireScripts 
-    
+    @livewireScripts
+
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    
+
     <script src="{{ asset('js/nav.js') }}"></script>
 
 </body>

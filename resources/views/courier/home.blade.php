02c0d5fc1dc5a8b5b5730b7e2306dcf8106f0471
GHoran <ghoranmanurung26038@gmail.com>
2024-06-06 08:30:50 +0700

    courier

--- resources/views/courier/home.blade.php
+++ resources/views/courier/home.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.appCourier')
 
 @push('head')
-    <link rel="stylesheet" href="{{ asset('css/admin/home.css') }}"> {{-- ini buat nyambungin home.css ke home blade nya --}}
+    <link rel="stylesheet" href="{{ asset('css/courier/home.css') }}"> {{-- ini buat nyambungin home.css ke home blade nya --}}
+    {{-- <link rel="stylesheet" href="{{ asset('css/home.css') }}"> ini buat nyambungin home.css ke home blade nya --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">
+
 @endpush
 
 @section('title', 'Courier Home')
@@ -13,8 +16,107 @@
     <div class="errorMessage">
         @include('widgets.errorMessage')
     </div>
-    <p class="h1">Welcome Courier</p>
-    <p>Home page</p>
+    <div class="content">
+        <img src="{{ asset('image/GrowceriesLogo.svg') }}" class="logoGrow" alt="" srcset="">
+        <div class="header">
+            <div class="title">Order List</div>
+            <form action="{{route('discover')}}" method="GET" id="search" data-aos="fade-right" data-aos-delay="200"
+        data-aos-duration="1000">
+
+        <div class="input-group">
+          <div class="search_container">
+            <i class="bi bi-search" id="magnifying" style="color: #050505 solid"></i>
+            <input name="search_Input" type="text" autocomplete="off" placeholder="Search your daily Growceries"
+              id="myInput" oninput="filterFunction()" class="form-control">
+            <button class="search_button">Search</button>
+          </div>
+        </div>
+        <div class="result_box">
+          <div id="dropdown_m0" class="dropdown m-0">
+            <div id="myDropdown" class="dropdown-content">
+              <div class="result_field">
+                {{-- @foreach ($products as $product)
+                <div name="search_Input" class="percontent">
+                  <a href="">
+                    <div id="result_field" class="media flex-wrap w-100 align-items-centerr">
+                      <i class="bi bi-search" id="magnifying2" style="color: #050505 solid"></i>
+                      <p class="ml-23" style="color: #050505">{{ $product->productDetail->productName }}</p>
+                    </div>
+                  </a>
+                </div>
+                @endforeach --}}
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </form>
+        </div>
+
+        <div class="option">
+            <div class="option_sub">
+                <p class="option_title">Pick Up Order</p>
+            </div>
+            <div class="option_sub">
+                <p class="option_title">Shipped Order</p>
+            </div>
+        </div>
+        <div class="listfield">
+            <div class="box">
+                <div class="top">
+                    <img class="icon_status" src="{{ asset('image/packed.svg')}}" alt="" srcset="">
+                    <div class="detail">
+                        <p class="status_title">Order is being packed</p>
+                        <p class="date">Purchased April 23, 2024</p>
+                        <p class="id_order">#ABC123BCA123</p>
+                    </div>
+                    <div class="price">Rp. 72.000</div>
+                </div>
+                <div class="line">
+                </div>
+                <div class="bottom">
+                    <button class="update_status">Order Pick Up</button>
+                </div>
+            </div>
+            <div class="box">
+                <div class="top">
+                    <img class="icon_status" src="{{ asset('image/shipped.svg')}}" alt="" srcset="">
+                    <div class="detail">
+                        <p class="status_title">Order is being shipped</p>
+                        <p class="date">Purchased April 23, 2024</p>
+                        <p class="id_order">#ABC123BCA123</p>
+                    </div>
+                    <div class="price">Rp. 72.000</div>
+                </div>
+                <div class="line">
+                </div>
+                <div class="bottom">
+                    <button class="update_status">Order Complete</button>
+                </div>
+            </div>
+            <div class="box">
+                <div class="top">
+                    <img class="icon_status" src="{{ asset('image/shipped.svg')}}" alt="" srcset="">
+                    <div class="detail">
+                        <p class="status_title">Order is being shipped</p>
+                        <p class="date">Purchased April 23, 2024</p>
+                        <p class="id_order">#ABC123BCA123</p>
+                    </div>
+                    <div class="price">Rp. 72.000</div>
+                </div>
+                <div class="line">
+                </div>
+                <div class="bottom">
+                    <button class="update_status">Order Complete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    {{-- <p class="h1">Welcome Courier</p>
+    <p>Home page</p> --}}
 
 
 @endsection
--- resources/views/layouts/appCourier.blade.php
+++ resources/views/layouts/appCourier.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> @yield('title') </title>    
+    <title> @yield('title') </title>
 
     {{-- Bootstrap CSS & JS--}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -29,22 +29,22 @@
 
 </head>
 <body>
-    @include('navigation.courier.nav')
+    {{-- @include('navigation.courier.nav') --}}
 
     @if (Route::currentRouteName() == 'courier.login')
         @yield('error')
         @yield('success')
         @yield('login')
     @else
-        @yield('content') 
+        @yield('content')
     @endif
 
     @include('navigation.footer')
-    
+
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    
+
     <script src="{{ asset('js/nav.js') }}"></script>
 
     @yield('script')

e71ef4344fd260cf374bd8d372734bc4c46c87d3
Delvin Setiamin Salim <delvin.setiamin.08@gmail.com>
2024-07-03 20:56:24 +0700

    pushing

--- routes/web.php
+++ routes/web.php
@@ -95,11 +95,12 @@ Route::group(['prefix' => 'admin'], function(){
         Route::get('product/{product}', [adminProductController::class, 'destroy'])->name('admin.delete');
         Route::get('product/{product}/edit', [adminProductController::class, 'edit'])->name('admin.editProduct');
         Route::post('product/{product}', [adminProductController::class, 'update'])->name('admin.updateProduct');
-        Route::get('orderAdmin', [adminOrderController::class, 'index'])->name('admin.orderAdmin');
         Route::get('profileAdmin', [adminProfileController::class, 'index'])->name('admin.profileAdmin');
         Route::post('profileAdmin', [adminProfileController::class, 'index'])->name('admin.profileAdmin');
         Route::put('product/{product}', [adminProductController::class, 'update'])->name('admin.updateProduct');
         Route::get('product/{product?}', [adminProductController::class, 'search'])->name('admin.searchProduct');
+        Route::get('orderAdmin', [adminOrderController::class, 'index'])->name('admin.orderAdmin');
+        Route::get('orderAdmin/{id}', [adminOrderController::class, 'detail'])->name('admin.orderDetail');
 
     });
 });

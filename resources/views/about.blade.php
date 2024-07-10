528e8389635d3ea217ae1ff5930250cdb6fb0587
AthaliaYechiel <122966304+AthaliaYechiel@users.noreply.github.com>
2024-07-10 11:17:59 +0700

    about us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,46 +1,53 @@
 @extends('layouts.app')
 
+
 @push('head')
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/about.css') }}">
+
 @endpush
 
-@section('title', 'About Us')
+@section('title', 'Contact Us')
 
 @section('content')
 
-    <div class="aboutus">
-        <div class="aboutunit">
-            <img src= "https://drive.google.com/thumbnail?id=1VY_8ILTsP8iz2cjjcae2CGFvSLHeR3Ah" alt="" width="300px">
-            <p>Name : Athalia Yechiel Tzeliang</p>
-            <p>NIM : 2602189562</p>
-            <p>Email : athalia.tzeliang@binus.ac.id</p>
-
-        </div>
-        <div class="aboutunit">
-            <img src="https://drive.google.com/thumbnail?id=1mXMVJd9FA79GDZtVT4aSjVUh0HpLrhcU" alt=""
-                width="300px">
-            <p>Name : Delvin Setiamin Salim</p>
-            <p>NIM : 2602189562</p>
-            <p>Email : delvin.salim@binus.ac.id</p>
-        </div>
-        <div class="aboutunit">
-            <img src="https://drive.google.com/thumbnail?id=1IVUYS0BdskPg0d8mZAhOc5agpZzCbwH8" alt="" width="300px">
-            <p>Name : Ghoran Fiodoli Manurung</p>
-            <p>NIM : 2602189562</p>
-            <p>Email : ghoran.manurung@binus.ac.id</p>
-        </div>
-        <div class="aboutunit">
-            <img src="https://drive.google.com/thumbnail?id=1nWckeJRTbZynfZ1vyXlZ04xRfFbX1Jt1" alt="" width="300px">
-            <p>Name : Jacky</p>
-            <p>NIM : 2602189562</p>
-            <p>Email : jacky007@binus.ac.id</p>
-        </div>
-        <div class="aboutunit">
-            <img src="https://drive.google.com/thumbnail?id=1-9VtC1SH7TTaXCcsejEtyn6SNFTYCXow" alt="" width="300px">
-            <p>Name : Marvel Jonathan Doloksaribu</p>
-            <p>NIM : 2602189562</p>
-            <p>Email : marvel.doloksaribu@binus.ac.id</p>
+<div class="banner-faq">
+    <div class="banner">
+        <img src="image/banner.png" alt="" class="full-img">
+        <div class="text1">
+            <h1>About Us</h1>
         </div>
     </div>
+</div>
+<div class="description">
+    <h1>What Is Growceries?</h1>
+    <img src="image/logoGrowceries.png" alt="" class="logoimg">
+    <p>"Growceries" is an innovative application specifically designed to facilitate the process of selling vegetables and fruit online. With existing features, Growceries allows consumers to buy fresh products directly from the source. With a user-friendly interface and secure payment system, Growceries is an ideal solution for those who value quality and sustainability in meeting their vegetable and fruit needs.</p>
+</div>
+<div class="contact">
+    <div class="desc">
+        <h1>Why Choose Us?</h1>
+        <p>Because we offer a comfortable shopping experience, quality product collection, friendly customer service, and our commitment to your satisfaction.</p>
+        <button id="contact-btn" onclick="location.href='{{ route('contactUs') }}'">Contact Us</button>
+    </div>
+    <div class="img">
+        <img src="image/basket-with-vegetables-png-clipart-image-5a1c5cb10dbcb8 1.png" alt="" class="img">
+    </div>
+</div>
+<div class="contact">
+    <div class="img">
+        <img src="image/image 8.png" alt="" class="img">
+    </div>
+    <div class="desc-store">
+        <h1>What We Provide?</h1>
+        <p>In GrowCeries, we provide a wide variety of fresh fruits and vegetables you need to stock your kitchen and home.</p>
+        <button id="store-btn" onclick="location.href='{{ route('discover') }}'">Store</button>
+    </div>
+</div>
+
+
+
 @endsection
 
+@section('script')
+    <script src="{{ asset('js/about.js') }}"></script>
+@endsection
